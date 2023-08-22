5c474977ba4c81f84418cd5b025620e473ed6241
Mathis <math.vaille@gmail.com>
2023-08-23 00:18:05 +0200

    Bug Fix

--- cursor.js
+++ cursor.js
@@ -8,9 +8,25 @@ window.addEventListener('mousemove', e => {
 
 function animateCursor() {
     const cursor = document.getElementById('cursor');
-    cursor.style.left = cursorX + 'px';
-    cursor.style.top = cursorY + 'px';
+    const targetElement = document.elementFromPoint(cursorX, cursorY);
+
+    // Vérifiez si targetElement est non nul
+    if (targetElement) {
+        const customCursor = getComputedStyle(targetElement).getPropertyValue('--custom-cursor').trim();
+
+        cursor.style.left = cursorX + 'px';
+        cursor.style.top = cursorY + 'px';
+
+        if (customCursor && customCursor !== 'none') {
+            cursor.style.backgroundImage = customCursor.replace(/["\s]/g, '');
+        } else {
+            cursor.style.backgroundImage = "url('./Icons/Souris.svg')";
+        }
+    }
+
     requestAnimationFrame(animateCursor);
 }
+
+
 
 animateCursor();
